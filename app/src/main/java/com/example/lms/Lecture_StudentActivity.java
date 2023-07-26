a59df318ad9b039f03a24efc64721ee8566988cc
Piumi274 <bhagyapiumi274@gmail.com>
2023-07-26 13:42:28 +0530

    Learning Management system using Android studio

---
+++ app/src/main/java/com/example/lms/Lecture_StudentActivity.java
@@ -0,0 +1,14 @@
+package com.example.lms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lecture_StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecture_student);
+    }
+}
